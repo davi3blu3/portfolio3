8f2e47457f150f1e26e6ef462e55e41df7ee0eee
Dave Harned <dharned@gmail.com>
2016-02-20 16:27:12 -0600

    adding section headers and basic layout

--- js/scroll.js
+++ js/scroll.js
@@ -1,2 +1,17 @@
 
+// Detect scroll passed home section, change nav menu position
+
+$(window).scroll(function() {
+
+    var aboutSect = $( ".about-section" );
+
+    if ($(window).scrollTop() > aboutSect.position().top) {
+      $('nav').addClass("nav-sticky");
+      $('nav').removeClass("nav-bottom");
+    } else {
+      $('nav').removeClass("nav-sticky");
+      $('nav').addClass("nav-bottom");                    
+    }
+});
+
 

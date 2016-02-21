6d3c65369f39c783c21c8207f8530005971e5c55
Dave Harned <dharned@gmail.com>
2016-02-21 16:05:52 -0600

    structure portfolio pieces in bootstrap

--- js/scroll.js
+++ js/scroll.js
@@ -3,9 +3,9 @@
 
 $(window).scroll(function() {
 
-    var aboutSect = $( ".about-section" );
+    var nextSect = $( ".code-section" );
 
-    if ($(window).scrollTop() > aboutSect.position().top) {
+    if ($(window).scrollTop() > nextSect.position().top) {
       $('nav').addClass("nav-sticky");
       $('nav').removeClass("nav-bottom");
     } else {

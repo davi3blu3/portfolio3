0384a00aec2ea1ecc37b33f4498ba493ae8c44a8
Dave Harned <dharned@gmail.com>
2016-02-21 20:51:07 -0600

    add jquery fade to nav

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,31 @@
 
-// Detect scroll passed home section, change nav menu position
+// Declare global variables
+var nextSect = $( ".code-section" ),
+navHeight = $( "nav" ).height(),
+previousScroll = 0;
 
 $(window).scroll(function() {
-
-    var nextSect = $( ".code-section" );
-
-    if ($(window).scrollTop() > nextSect.position().top) {
-      $('nav').addClass("nav-sticky");
+  
+    // declare function variable
+    var currentScroll = $(this).scrollTop();
+  
+    // Detect when scroll passes home section - nav height, make nav sticky
+    if ($(window).scrollTop() > nextSect.position().top - navHeight) {
       $('nav').removeClass("nav-bottom");
+      $('nav').addClass("nav-sticky");
+      
+      // past home section, scroll direction affects navbar appearance
+      if (currentScroll > previousScroll) {
+        $('nav').fadeOut(1600);
+      } else {
+        $('nav').fadeIn('slow');
+      }
     } else {
       $('nav').removeClass("nav-sticky");
-      $('nav').addClass("nav-bottom");                    
+      $('nav').addClass("nav-bottom");
     }
+    previousScroll = currentScroll;
+
 });
 
 
